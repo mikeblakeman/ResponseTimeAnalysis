84b89b5e3f7daed9fb0405e07dac9fc12d113423
Blakeman, Mike <Michael.Blakeman@garmin.com>
2019-10-10 09:26:30 -0500

    Boilerplate poject setup
    Boilerplate project setup with SwiftLint and Promises CocoaPods

---
+++ Response Time Analysis/Pods/Target Support Files/Pods-Response Time Analysis/Pods-Response Time Analysis-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Response_Time_Analysis : NSObject
+@end
+@implementation PodsDummy_Pods_Response_Time_Analysis
+@end
---
+++ Response Time Analysis/Response Time Analysis/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Response Time Analysis
+//
+//  Created by Blakeman, Mike on 10/10/19.
+//  Copyright © 2019 Blakeman, Mike. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
